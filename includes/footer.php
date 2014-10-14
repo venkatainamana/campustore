5ece917d148f25244d64fdc18fed778a189c45e9
StevenLiu <Steven@StevenLius-MacBook-Air.local>
2014-10-13 20:57:10 -0400

    add header.php and footer.php and share them with all other pages

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+    <div class="footer">
+	<h3>Copyright &copy; CIS 525 Group 6</h3></center>
+    </div>
+
+  </div>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WebShelf</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+    <div class="container">
+        <a href="index.php" id="logo">WebShelf</a>
+
